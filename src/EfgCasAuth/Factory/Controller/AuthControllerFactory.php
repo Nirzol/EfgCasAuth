eb769ea5fbed637c96349d32693dc4074b33036d
Nirzol <grondin.eric@gmail.com>
2016-03-24 11:31:36 +0100

    First commit - init project

---
+++ src/EfgCasAuth/Factory/Controller/AuthControllerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace EfgCasAuth\Factory\Controller;
+
+use EfgCasAuth\Controller\AuthController;
+use Zend\Mvc\Controller\ControllerManager;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class AuthControllerFactory implements FactoryInterface
+{
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        /* @var $serviceLocator ControllerManager */
+        $sm = $serviceLocator->getServiceLocator();
+
+        $authService = $sm->get('Zend\Authentication\AuthenticationService');
+
+        $config = $sm->get('Config');
+
+        $authController = new AuthController($authService, $config['cas']);
+
+        return $authController;
+    }
+}
